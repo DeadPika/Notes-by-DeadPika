0ca9df69d20d883ace4899a28575ac0f875d3582
DeadPika <xleb338@gmail.com>
2025-04-11 23:56:42 +0500

    Добавьте файлы проекта.

---
+++ Notes.Persistence/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Notes.Application.Interfaces;
+
+namespace Notes.Persistence
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistence(this IServiceCollection service, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DbConnection");
+            service.AddDbContext<NotesDbContext>(options =>
+            {
+                options.UseSqlite(connectionString);
+            });
+            service.AddScoped<INotesDbContext>(provider => provider.GetService<NotesDbContext>()!);
+            return service;
+        }
+    }
+}
---
+++ Notes.WebApi/Program.cs
@@ -0,0 +1,74 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Notes.Persistence;
+using Notes.Application;
+using Notes.Application.Common.Mapping;
+using Notes.Application.Interfaces;
+using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Настройка сервисов (аналог ConfigureServices из Startup)
+builder.Services.AddAutoMapper(config =>
+{
+    config.AddProfile(new AssemblyMappingProfile(Assembly.GetExecutingAssembly()));
+    config.AddProfile(new AssemblyMappingProfile(typeof(INotesDbContext).Assembly));
+});
+
+builder.Services.AddApplication();
+builder.Services.AddPersistence(builder.Configuration);
+builder.Services.AddControllers();
+
+var cs = builder.Configuration.GetSection("DbConnection");
+
+
+builder.Services.AddDbContext<NotesDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DbConnection")));
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyHeader();
+        policy.AllowAnyMethod();
+        policy.AllowAnyOrigin();
+    });
+});
+
+var app = builder.Build();
+
+// Инициализация базы данных (аналог кода из Main)
+using (var scope = app.Services.CreateScope())
+{
+    var serviceProvider = scope.ServiceProvider;
+    try
+    {
+        var context = serviceProvider.GetRequiredService<NotesDbContext>();
+        DbInitializer.Initialize(context);
+    }
+    catch (Exception exception)
+    {
+        // Здесь можно добавить логирование ошибки, например:
+        var logger = serviceProvider.GetRequiredService<ILogger<Program>>();
+        logger.LogError(exception, "An error occurred while initializing the database.");
+    }
+}
+
+// Настройка пайплайна (аналог Configure из Startup)
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseRouting();
+app.UseHttpsRedirection();
+app.UseCors("AllowAll");
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
+app.Run();
