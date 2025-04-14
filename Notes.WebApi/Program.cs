9f4cc63d5ccdfa907b17908fa74ae9a5b0a6ac53
DeadPika <xleb338@gmail.com>
2025-04-14 20:41:37 +0500

    Добавление Fluent Validation.

--- Notes.WebApi/Program.cs
+++ Notes.WebApi/Program.cs
@@ -1,6 +1,3 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Notes.Persistence;
 using Notes.Application;
 using Notes.Application.Common.Mapping;
